74801361707f96aaf75823fbc10e0e734474b7ce
Ash <ash770503@gmail.com>
2026-05-13 16:37:23 +0530

    add TodayReport with settlement + close day flow

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Dashboard from './admin/Dashboard'
 import MenuManager from './admin/MenuManager'
 import TableManager from './admin/TableManager'
 import Reports from './admin/Reports'
+import TodayReport from './admin/TodayReport'
 import MenuPage from './customer/MenuPage'
 import OrderConfirmation from './customer/OrderConfirmation'
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/admin/dashboard" element={<Dashboard />} />
         <Route path="/admin/menu" element={<MenuManager />} />
         <Route path="/admin/tables" element={<TableManager />} />
+        <Route path="/admin/today-report" element={<TodayReport />} />
         <Route path="/admin/reports" element={<Reports />} />
         <Route path="/menu" element={<MenuPage />} />
         <Route path="/order-confirmation" element={<OrderConfirmation />} />
