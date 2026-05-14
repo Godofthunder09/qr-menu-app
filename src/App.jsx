8c7f2da0f81b351a1cc804b45954b7e2e2fc9632
Ash <ash770503@gmail.com>
2026-05-14 14:18:11 +0530

    load restaurant settings from DB in dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MenuManager from './admin/MenuManager'
 import TableManager from './admin/TableManager'
 import Reports from './admin/Reports'
 import TodayReport from './admin/TodayReport'
+import Settings from './admin/Settings'
 import MenuPage from './customer/MenuPage'
 import OrderConfirmation from './customer/OrderConfirmation'
 
@@ -19,6 +20,7 @@ function App() {
         <Route path="/admin/tables" element={<TableManager />} />
         <Route path="/admin/today-report" element={<TodayReport />} />
         <Route path="/admin/reports" element={<Reports />} />
+        <Route path="/admin/settings" element={<Settings />} />
         <Route path="/menu" element={<MenuPage />} />
         <Route path="/order-confirmation" element={<OrderConfirmation />} />
       </Routes>
