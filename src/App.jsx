3ad3641ad3687b551c3a3285160a55673f2178e0
Ash <ash770503@gmail.com>
2026-05-21 13:40:03 +0530

    Add discount, bill preview, and Epson printer support

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import TableManager from './admin/TableManager'
 import Reports from './admin/Reports'
 import TodayReport from './admin/TodayReport'
 import Settings from './admin/Settings'
+import ReportTablewise from './admin/Report/Report-Tablewise'
 import MenuPage from './customer/MenuPage'
 import OrderConfirmation from './customer/OrderConfirmation'
 
@@ -20,6 +21,7 @@ function App() {
         <Route path="/admin/tables" element={<TableManager />} />
         <Route path="/admin/today-report" element={<TodayReport />} />
         <Route path="/admin/reports" element={<Reports />} />
+        <Route path="/admin/reports/tablewise" element={<ReportTablewise />} />
         <Route path="/admin/settings" element={<Settings />} />
         <Route path="/menu" element={<MenuPage />} />
         <Route path="/order-confirmation" element={<OrderConfirmation />} />
