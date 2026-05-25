caa6b5624bbb37afadc4d35c490374761712de95
Ash <ash770503@gmail.com>
2026-05-26 00:52:41 +0530

    merge all report tabs into single Reports.jsx - fix white page

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,8 @@ import TableManager from './admin/TableManager'
 import Reports from './admin/Reports'
 import TodayReport from './admin/TodayReport'
 import Settings from './admin/Settings'
-import ReportTablewise from './admin/Report/Report-Tablewise'
 import MenuPage from './customer/MenuPage'
 import OrderConfirmation from './customer/OrderConfirmation'
-import ReportToday from './admin/Report/Report-Today'
-import ReportDateRange from './admin/Report/Report-DateRange'
-import ReportCategory from './admin/Report/Report-Category'
-import ReportDiscounts from './admin/Report/Report-Discounts'
-import ReportSettlement from './admin/Report/Report-Settlement'
 import ProtectedRoute from './admin/ProtectedRoute'
 
 function App() {
@@ -27,15 +21,9 @@ function App() {
         <Route path="/admin/tables" element={<TableManager />} />
         <Route path="/admin/today-report" element={<TodayReport />} />
         <Route path="/admin/reports" element={<Reports />} />
-        <Route path="/admin/reports/tablewise" element={<ReportTablewise />} />
         <Route path="/admin/settings" element={<Settings />} />
         <Route path="/menu" element={<MenuPage />} />
         <Route path="/order-confirmation" element={<OrderConfirmation />} />
-        <Route path="/admin/report/today" element={<ProtectedRoute><ReportToday /></ProtectedRoute>} />
-        <Route path="/admin/report/daterange" element={<ProtectedRoute><ReportDateRange /></ProtectedRoute>} />
-        <Route path="/admin/report/category" element={<ProtectedRoute><ReportCategory /></ProtectedRoute>} />
-        <Route path="/admin/report/discounts" element={<ProtectedRoute><ReportDiscounts /></ProtectedRoute>} />
-        <Route path="/admin/report/settlement" element={<ProtectedRoute><ReportSettlement /></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
   )
